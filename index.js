e6c060611f8f6f2fb7ffbc5e8868762c16ddf4bb
Carl_Javier <ccjavier87@gmail.com>
2023-02-12 11:07:06 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+
+function findMatching  (data, match) {
+    return data.filter(function (name){
+        return name.toLowerCase() === match.toLowerCase()
+    })
+}
+
+function fuzzyMatch (data, match) {
+    return data.filter(
+        name => name[0] === match[0]
+    )
+}
+
+function matchName(data, name) {
+    return data.filter(
+        function (hometown) {
+            return hometown.name === name
+        }
+    )
+}
